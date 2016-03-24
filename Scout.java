beec5cacaf5ac4c2b206d7d11c006af64acf06d9
Ryan Fowler <RyanSFowler@users.noreply.github.com>
2016-03-24 10:31:59 -0400

    Added files via upload

---
+++ Scout.java
@@ -0,0 +1,276 @@
+// specify the package
+package model;
+
+// system imports
+import java.sql.SQLException;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.Vector;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.swing.JFrame;
+
+
+
+// project imports
+import exception.InvalidPrimaryKeyException;
+import database.*;
+import impresario.IView;
+import userinterface.*;
+import userinterface.BookView;
+
+
+/** The class containing the Account for the ATM application */
+//==============================================================
+public class Scout extends EntityBase implements IView
+{
+	private static final String myTableName = "Scout";
+
+	protected Properties dependencies;
+	protected Stage myStage;
+	protected Librarian myTLC;
+
+	// GUI Components
+
+	private String updateStatusMessage = "";
+
+	// constructor for this class
+	//----------------------------------------------------------
+	public Scout(int scoutId)
+		throws InvalidPrimaryKeyException
+	{
+		super(myTableName);
+
+		setDependencies();
+		String query = "SELECT * FROM " + myTableName + " WHERE (bookId = " + scoutId + ")";
+
+		Vector<Properties> allDataRetrieved = getSelectQueryResult(query);
+
+		// You must get one account at least
+		if (allDataRetrieved != null)
+		{
+			int size = allDataRetrieved.size();
+
+			// There should be EXACTLY one bookIds. More than that is an error
+			if (size != 1)
+			{
+				throw new InvalidPrimaryKeyException("Multiple scouts matching id : "
+					+ scoutId + " found.");
+			}
+			else
+			{
+				// copy all the retrieved data into persistent state
+				Properties retrievedScoutData = allDataRetrieved.elementAt(0);
+				persistentState = new Properties();
+
+				Enumeration allKeys = retrievedScoutData.propertyNames();
+				while (allKeys.hasMoreElements() == true)
+				{
+					String nextKey = (String)allKeys.nextElement();
+					String nextValue = retrievedScoutData.getProperty(nextKey);
+
+					if (nextValue != null)
+					{
+						persistentState.setProperty(nextKey, nextValue);
+					}
+				}
+
+			}
+		}
+		// If no Book found for this id, throw an exception
+		else
+		{
+			throw new InvalidPrimaryKeyException("No scout matching id : "
+				+ scoutId  + " found.");
+		}
+	}
+
+	// Can also be used to create a NEW Book (if the system it is part of
+	// allows for a new book to be set up)
+	//----------------------------------------------------------
+	public Scout(Properties props)
+	{
+		super(myTableName);
+
+		setDependencies();
+		setData(props);
+	}
+
+	public Scout(TLC t)
+		{
+		super(myTableName);
+		myStage = MainStageContainer.getInstance();
+		myTLC = t;
+
+		}
+
+
+	//-----------------------------------------------------------------------------------
+	public void setData(Properties props)
+	{
+		persistentState = new Properties();
+		Enumeration allKeys = props.propertyNames();
+		while (allKeys.hasMoreElements() == true)
+		{
+			String nextKey = (String)allKeys.nextElement();
+			String nextValue = props.getProperty(nextKey);
+
+			if (nextValue != null)
+			{
+				persistentState.setProperty(nextKey, nextValue);
+			}
+		}
+	}
+	//-----------------------------------------------------------------------------------
+
+	private void setDependencies()
+	{
+		dependencies = new Properties();
+
+		myRegistry.setDependencies(dependencies);
+	}
+
+	//----------------------------------------------------------
+	public Object getState(String key)
+	{
+		if (key.equals("UpdateStatusMessage") == true)
+			return updateStatusMessage;
+
+		return persistentState.getProperty(key);
+	}
+
+	//----------------------------------------------------------------
+	public void stateChangeRequest(String key, Object value)
+	{
+
+		myRegistry.updateSubscribers(key, this);
+	}
+
+	/** Called via the IView relationship */
+	//----------------------------------------------------------
+	public void updateState(String key, Object value)
+	{
+		stateChangeRequest(key, value);
+	}
+
+
+
+
+	//-----------------------------------------------------------------------------------
+	public static int compare(Scout a, Scout b)
+	{
+		String aNum = (String)a.getState("scoutId");//this or title
+		String bNum = (String)b.getState("scoutId");
+
+		return aNum.compareTo(bNum);
+	}
+
+	//-----------------------------------------------------------------------------------
+	//****Needs to be modified.
+	public void update()
+	{
+		updateStateInDatabase();
+	}
+
+	//-----------------------------------------------------------------------------------
+	private void updateStateInDatabase()
+	{
+		try
+		{
+			if (persistentState.getProperty("scoutId") != null)
+			{
+				Properties whereClause = new Properties();
+				whereClause.setProperty("scoutId",
+				persistentState.getProperty("scoutId"));
+				updatePersistentState(mySchema, persistentState, whereClause);
+				updateStatusMessage = "Scout data for scout number : " + persistentState.getProperty("scoutId") + " updated successfully in database!";
+				System.out.println("Trying to update Scout");
+			}
+			else
+			{
+
+				Integer scoutId =
+					insertAutoIncrementalPersistentState(mySchema, persistentState);
+				persistentState.setProperty("scoutId", "" + scoutId.intValue());
+				System.out.println("Scout data for new scout installed successfully in database");
+				updateStatusMessage = "Scout data for new scout : " +  persistentState.getProperty("scoutId")
+					+ "installed successfully in database!";
+			}
+			Scene currentScene = (Scene)myViews.get("ScoutView");
+
+			if (currentScene != null)
+			{
+					((ScoutView)currentScene.getRoot()).displayMessage("Scout saved successfully!");
+			}
+
+		}
+		catch (SQLException ex)
+		{
+			System.out.println("Error in installing Scout data in database!");
+			updateStatusMessage = "Error in installing Scout data in database!";
+
+			Scene currentScene = (Scene)myViews.get("ScoutView");
+
+			if (currentScene != null)
+			{
+					((ScoutView)currentScene.getRoot()).displayErrorMessage("ERROR in Scout save!");
+			}
+		}
+	}
+
+	public Vector<String> getEntryListView()
+	{
+		Vector<String> v = new Vector<String>();
+
+		v.addElement(persistentState.getProperty("scoutId"));
+		v.addElement(persistentState.getProperty("firstName"));
+		v.addElement(persistentState.getProperty("middleInitial"));
+		v.addElement(persistentState.getProperty("lastName"));
+		v.addElement(persistentState.getProperty("dob"));
+		v.addElement(persistentState.getProperty("phoneNum"));
+		v.addElement(persistentState.getProperty("email"));
+		v.addElement(persistentState.getProperty("status"));
+
+		return v;
+	}
+
+	//-----------------------------------------------------------------------------------
+	protected void initializeSchema(String tableName)
+	{
+		if (mySchema == null)
+		{
+			mySchema = getSchemaInfo(tableName);
+		}
+	}
+
+	//-------------------------------------------------------------------------
+	public String toString()
+	{
+		return persistentState.toString();
+	}
+	//-------------------------------------------------------------------------
+	//NEW METHODS 2-21-16------------------------------------------------------
+	public void createAndShowScoutView()
+	{
+		Scene currentScene = (Scene)myViews.get("ScoutView");
+
+		if (currentScene == null)
+		{
+			// create our initial view
+			View newView = new ScoutView(this); // USE VIEW FACTORY
+			currentScene = new Scene(newView);
+			myViews.put("ScoutView", currentScene);
+		}
+
+		swapToView(currentScene);
+
+	}
+
+	public void done()
+	{
+		myTLC.transactionDone();
+
+	}
+}
---
+++ ScoutCollection.java
@@ -0,0 +1,229 @@
+// specify the package
+package model;
+
+// system imports
+import java.util.Properties;
+import java.util.Vector;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+// project imports
+import exception.InvalidPrimaryKeyException;
+import event.Event;
+import database.*;
+import impresario.IView;
+import userinterface.BookView;
+import userinterface.MainStageContainer;
+import userinterface.View;
+import userinterface.ViewFactory;
+import userinterface.BookCollectionView;
+//import userinterface.BookCollectionView;
+
+
+/** The class containing the BookCollection */
+//==============================================================
+public class ScoutCollection  extends EntityBase implements IView
+{
+	private static final String myTableName = "Scout";
+
+	private Vector<Book> scouts;
+	protected Stage myStage;
+	protected TLC myTLC;
+
+	// GUI Components
+
+	// constructor for this class
+	//----------------------------------------------------------
+	public ScoutCollection()   //(BookHolder cust) for param is removed
+	{
+		super(myTableName);
+		scouts = new Vector<Scout>();
+	}
+
+	public ScoutCollection(TLC t)
+	{
+		super(myTableName);
+		myStage = MainStageContainer.getInstance();
+		myTLC = t;
+	}
+	//----------------------------------------------------------------------------------
+	public void addScout(Scout a)
+	{
+		//book.add(a);
+		int index = findIndexToAdd(a);
+		scouts.insertElementAt(a,index); // To build up a collection sorted on some key
+	}
+	//----------------------------------------------------------------------------------
+	private int findIndexToAdd(Scout b)
+	{
+		//users.add(u);
+		int low=0;
+		int high = scouts.size()-1;
+		int middle;
+
+		while (low <=high)
+		{
+			middle = (low+high)/2;
+
+			Scout midSession = books.elementAt(middle);
+
+			int result = Scout.compare(b,midSession);
+
+			if (result ==0)
+			{
+				return middle;
+			}
+			else if (result<0)
+			{
+				high=middle-1;
+			}
+			else
+			{
+				low=middle+1;
+			}
+
+
+		}
+		return low;
+	}
+
+
+	/**
+	 *
+	 */
+	//----------------------------------------------------------
+	public Object getState(String key)
+	{
+		if (key.equals("Scouts"))
+			return scouts;
+		else
+		if (key.equals("ScoutList"))
+			return this;
+		return null;
+	}
+
+	//----------------------------------------------------------------
+	public void stateChangeRequest(String key, Object value)
+	{
+
+		myRegistry.updateSubscribers(key, this);
+	}
+
+	//----------------------------------------------------------
+	public Scout retrieve(String scoutId)
+	{
+		Scout retValue = null;
+		for (int cnt = 0; cnt < books.size(); cnt++)
+		{
+			Scout nextScout = scouts.elementAt(cnt);
+			String nextScoutId = (String)nextBk.getState("ScoutId");
+			if (nextScoutId.equals(scoutId) == true)
+			{
+				retValue = nextScout;
+				return retValue; // we should say 'break;' here
+			}
+		}
+
+		return retValue;
+	}
+
+	/** Called via the IView relationship */
+	//----------------------------------------------------------
+	public void updateState(String key, Object value)
+	{
+		stateChangeRequest(key, value);
+	}
+
+	//------------------------------------------------------
+	protected void createAndShowView()
+	{
+
+		Scene localScene = myViews.get("ScoutCollectionView");
+
+		if (localScene == null)
+		{
+				// create our new view
+				View newView = ViewFactory.createView("ScoutCollectionView", this);
+				localScene = new Scene(newView);
+				myViews.put("ScoutCollectionView", localScene);
+		}
+		// make the view visible by installing it into the frame
+		swapToView(localScene);
+
+	}
+
+	//-----------------------------------------------------------------------------------
+	protected void initializeSchema(String tableName)
+	{
+		if (mySchema == null)
+		{
+			mySchema = getSchemaInfo(tableName);
+		}
+	}
+
+
+	//----------------------------------------------------------------------------------
+
+
+
+	public void findBooksWithTitleLike(String title)
+	{
+
+		String query = "SELECT * FROM " + myTableName + " WHERE (title LIKE '%" + title + "%')";
+
+		Vector allDataRetrieved = getSelectQueryResult(query);
+
+		if (allDataRetrieved != null)
+		{
+			books = new Vector<Book>();
+
+			for (int cnt = 0; cnt < allDataRetrieved.size(); cnt++)
+			{
+				Properties nextBookData = (Properties)allDataRetrieved.elementAt(cnt);
+
+				Book book = new Book(nextBookData);
+
+				if (book != null)
+				{
+					addBook(book);
+				}
+			}
+
+		}
+		else{}
+
+	}
+
+	//---------------------------------------------------------------
+	public void display()
+	{
+		for (int i = 0; i < scouts.size(); i++)
+		{
+			System.out.println(scouts.get(i));
+		}
+	}
+	//---------------------------------------------------------------
+	public void createAndShowScoutCollectionView()
+	{
+		Scene currentScene = (Scene)myViews.get("ScoutCollectionView");
+
+		if (currentScene == null)
+		{
+			// create our initial view
+			View newView = new ScoutCollectionView(this); // USE VIEW FACTORY
+			currentScene = new Scene(newView);
+			myViews.put("ScoutCollectionView", currentScene);
+		}
+
+		swapToView(currentScene);
+
+
+	}
+
+	public void done()
+	{
+		myTLC.transactionDone();
+
+	}
+}
